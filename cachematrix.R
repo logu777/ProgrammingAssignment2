f5677960788e9bfd640d4dd4058ff520b31dfeb2
Logeeswaran <65697530+logu777@users.noreply.github.com>
2020-06-04 08:33:49 +0530

    Updating the template R file with my function
    Made slight changes in the actual mean function in order to attain this function

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Creating a special "matrix"
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inver <- NULL
+    set <- function(y) {
+        x <<- y
+        inver <<- NULL
+    }
+    get <- function() x
+    setcache <- function(solve) inver <<- solve
+    getcache <- function() inver
+    list(set = set, get = get,
+         setcache = setcache,
+         getcache = getcache)
 }
 
 
-## Write a short comment describing this function
+## creating inverse of a matrix and cache it
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+            inver <- x$getcache()
+        if(!is.null(inver)) {
+            message("getting cached data")
+            return(inver)
+        }
+        data <- x$get()
+        inver <- solve(data, ...)
+        x$setcache(inver)
+        inver
+    
 }
